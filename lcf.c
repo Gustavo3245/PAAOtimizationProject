31521bd475fd68aa1242c9801cb13a37e1b7f385
Gustavo3245 <Gustavomoreiradev3245@gmail.com>
2026-01-12 20:15:10 -0300

    Feat: Implementing LCF algorithm for revert finish aulas

--- lcf.c
+++ lcf.c
@@ -1,4 +1,5 @@
 #include "types.h"
+#include <stdio.h>
 
 
 // Last Class Finish
@@ -27,5 +28,21 @@ void BurstTimeLast(Aula aulas[], int size){
 }
 
 void lastClassFinish(Aula aulas[], int size){
+    BurstTimeLast(aulas, size);
 
+    printf("--- Cronograma LCF (Terminam mais tarde) ---\n");
+
+    int total = 0;
+    int primeiraAula = 100;
+
+    for (int i = 0; i < size; i++) { 
+        if (aulas[i].endTime <= primeiraAula) {
+            printf("Aula: %s | %dh - %dh\n, the aula Finish in %dh", aulas[i].name, 
+                   aulas[i].startTime,aulas[i].endTime, aulas[i].endTime);
+            
+            primeiraAula = aulas[i].startTime;
+            total++;
+        } 
+    }
+    printf("Total de aulas possiveis: %d \n\n", total);
 }
--- main.c
+++ main.c
@@ -40,5 +40,6 @@ int main(int argc, char *argv[]){
 
     BurstTimeLast(lista, size);
     printList(lista,  size);
+    lastClassFinish(lista, size);
     
 }
