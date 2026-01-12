cea919920b07671c18cd7b27e8c7eaa7c4e85fa0
Gustavo3245 <Gustavomoreiradev3245@gmail.com>
2026-01-12 19:24:37 -0300

    Feat: implementing type and main files

---
+++ main.c
@@ -0,0 +1,41 @@
+#include "types.h"
+#include <stdio.h>
+
+
+void printList(Aula aulas[], int size){
+    printf("---------------------------------------- \n");
+    for (int value = 0; value <= size - 1; value++) {
+        printf(" Aula: %s, Start In: %d, Finish In: %d, Description: %s \n", 
+               aulas[value].name, aulas[value].startTime, aulas[value].endTime, aulas[value].description);
+    }
+    printf("----------------------------------------\n");
+
+}
+
+int main(int argc, char *argv[]){
+    
+    Aula lista[] = {
+        {"Processos", 10, 12, "description"},
+        {"Requisitos", 8, 11, "description"},
+        {"processo e analise de algoritmos", 11, 13, "description"},
+        {"Gerencia", 12, 14, "description"},
+        {"Cimento 2", 9, 10, "description"},
+        {"fundamentos de banco de dados", 8, 10, "description"},
+        {"Inteligencia artificial", 8, 14, "description"},
+        {"Programação Orientada A Objetos", 10, 11, "description"}
+    };
+
+    // quantidade de structs (elementos na lista);
+    //   int size = sizeof(lista) / sizeof(lista[0]);
+    int size = 8;
+
+    printList(lista, size);
+
+    BurstTime(lista,  size);
+    ShortestJobFirst(lista,  size);
+
+    BurstTimeFinal(lista,  size);
+    printList(lista, size);
+    EarliestFinishTime(lista, size);
+    
+}
---
+++ types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// definindo a estrutura que compoem uma aula.
+
+typedef struct {
+    char name[50];
+    int startTime;
+    int endTime;
+    char description[50];
+}Aula;
+
+void BurstTime(Aula aulas[], int size);
+void ShortestJobFirst(Aula aulas[], int size);
+void BurstTimeFinal(Aula aulas[], int size);
+void EarliestFinishTime(Aula aulas[], int size);
+
+
+#endif // !TYPES.H
